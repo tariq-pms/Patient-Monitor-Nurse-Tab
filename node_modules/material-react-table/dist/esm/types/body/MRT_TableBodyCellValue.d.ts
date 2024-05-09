77e486e0e02a664137b3a734d4fe1a3b22380e88
Tariq Ahmed <tariq@pmsind.com>
2024-05-09 11:47:06 +0530

    removed node_modules

--- node_modules/material-react-table/dist/esm/types/body/MRT_TableBodyCellValue.d.ts
+++
@@ -1,7 +0,0 @@
-import { type MRT_Cell, type MRT_TableInstance } from '../types';
-interface Props {
-    cell: MRT_Cell;
-    table: MRT_TableInstance;
-}
-export declare const MRT_TableBodyCellValue: ({ cell, table }: Props) => import("react/jsx-runtime").JSX.Element;
-export {};
